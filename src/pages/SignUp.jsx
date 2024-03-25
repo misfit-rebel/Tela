f8c018d1fe3a7daf37b5f2570eee5d865e4b8c51
misfit-rebel <hamza.misfit.rebel10@gmail.com>
2024-03-25 15:50:05 +0500

    proposals added

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,18 +1,22 @@
 // import CompleteProfile from '../components/CompleteProfile';
-import CreateAccount from '../components/CreateAccount';
-import JoinAsACustomer from '../components/JoinAsACustomer';
-import ProfileStep1 from '../components/ProfileStep1';
-import ProfileStep2 from '../components/ProfileStep2';
+// import CreateAccount from '../components/CreateAccount';
+// import JoinAsACustomer from '../components/JoinAsACustomer';
+// import ProfileStep1 from '../components/ProfileStep1';
+// import ProfileStep2 from '../components/ProfileStep2';
+import PlaceProposal from './PlaceProposal';
+import SubmittedProposals from './SubmittedProposals';
 
 const SignUp = () => {
   return (
     <>
-      {/* <JoinAsACustomer /> */}
-      {/* <CreateAccount /> */}
-      {/* <CompleteProfile /> */}
-      {/* <ProfileStep1 /> */}
-      <ProfileStep2 />
+      {/* <JoinAsACustomer />
       <CreateAccount />
+      <CompleteProfile />
+      <ProfileStep1 />
+      <ProfileStep2 />
+      <CreateAccount /> */}
+      {/* <PlaceProposal /> */}
+      <SubmittedProposals />
     </>
   );
 };
